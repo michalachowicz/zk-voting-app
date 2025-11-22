693d672027c8bfa900acf852c23acf2ed4fe1ddc
Michał Lachowicz <michalachowicz01@gmail.com>
2025-11-22 15:10:16 +0100

    add secret generation

--- website/logic.js
+++ website/logic.js
@@ -14,6 +14,9 @@ const ARBITRUM_CONFIG = {
     blockExplorerUrls: ['https://arbiscan.io/']
 };
 
+const LOGIN_MESSAGE = "Sign this message to generate user secret"
+let userSecret;
+
 async function switchToArbitrum() {
     await window.ethereum.request({
         method: 'wallet_switchEthereumChain',
@@ -21,15 +24,21 @@ async function switchToArbitrum() {
     });
 }
 
-connectButton.addEventListener('click', async () => {
+async function logIn() {
     const provider = new ethers.BrowserProvider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
     await switchToArbitrum();
     const signer = await provider.getSigner();
+    const signature = await signer.signMessage(LOGIN_MESSAGE);
+    console.log('signature: ', signature);
+    userSecret = ethers.keccak256(signature);
+    console.log(userSecret);
     const address = await signer.getAddress();
     output.innerText = address;
 
     const blockNumber = await provider.getBlockNumber();
 
     output.innerText = `Address: ${address}\nActual block number: ${blockNumber}`;
-});
+}
+
+connectButton.addEventListener('click', async () => logIn());

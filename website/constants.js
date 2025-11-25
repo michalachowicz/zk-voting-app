106b0d18e6398a2dfc188166f92d1232a5b9de07
Michał Lachowicz <michalachowicz01@gmail.com>
2025-11-25 05:37:23 +0100

    commit function

--- website/constants.js
+++ website/constants.js
@@ -1,5 +1,7 @@
 const ARBITRUM_CHAIN_ID = '0xa4b1';
 const LOGIN_MESSAGE = "Sign this message to generate user secret"
+const SALT_MESSAGE = "Sign this message to generate salt for your commit for voting - RoundId: "
+const P = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
 const CONTRACT_ADDRESS = "0xd53a88883646e2EFdFdeF5566C6A1CDe8f2Be30c";
 const CONTRACT_ABI = [
--- website/logic.js
+++ website/logic.js
@@ -10,8 +10,9 @@ const options = document.getElementById('options');
 const addRoundButton = document.getElementById('addRoundButton');
 const findPathButton = document.getElementById('findPathButton');
 const choosedOption = document.getElementById('choosedOption');
+const commitButton = document.getElementById('commitButton')
 
-let provider, signer, contract, userSecret;
+let provider, signer, contract, userSecret, userId;
 
 async function switchToArbitrum() {
     await window.ethereum.request({
@@ -27,12 +28,17 @@ async function logIn() {
 
     signer = await provider.getSigner();
     const signature = await signer.signMessage(LOGIN_MESSAGE);
-    userSecret = ethers.utils.keccak256(signature);
+    userSecret = window.poseidon([signature]);
+    userSecret = window.poseidon.F.toObject(userSecret);
+    userId = window.poseidon([userSecret]);
+    userId = window.poseidon.F.toObject(userId);
+    userId = ethers.utils.hexZeroPad("0x" + userId.toString(16), 32);
 
     contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
 
     console.log('signature: ', signature);
-    console.log(userSecret);
+    console.log("secret: ", userSecret);
+    console.log("ID: ", userId)
     console.log(contract.address);
     const owner = await contract.owner();
     console.log(owner);
@@ -110,11 +116,10 @@ async function findMerklePath() {
         throw new Error("No file with Merkle Tree found!");
     }
 
-    // 2. Odczytujemy jako tekst
     const text = await response.text();
     const merkleTree = text.split('\n');
     console.log(merkleTree);
-    const indentifierIndex = merkleTree.findIndex(value => value === userSecret);
+    const indentifierIndex = merkleTree.findIndex(value => value === userId);
     console.log(indentifierIndex);
     let path = [];
     let sides = [];
@@ -130,12 +135,57 @@ async function findMerklePath() {
         }
         currentIndex = Math.floor((currentIndex - 1) / 2);
     }
+    return { path, sides };
+}
+
+async function commit() {
+    const roundId = await contract.roundsCount() - 1;
+    const option = ethers.utils.formatBytes32String("tak");
+    let nullifier = window.poseidon([userSecret, roundId]);
+    nullifier = window.poseidon.F.toObject(nullifier);
+    nullifier = ethers.utils.hexZeroPad("0x" + nullifier.toString(16), 32);
+    const salt = await signer.signMessage(SALT_MESSAGE + roundId.toString());
+    const hashedSalt = ethers.utils.keccak256(salt);
+    const commitmentEncoded = ethers.utils.defaultAbiCoder.encode(
+        ["bytes32", "bytes32", "uint", "bytes32"], [option, nullifier, roundId, hashedSalt]
+    )
+    const commitmentHashed = ethers.utils.keccak256(commitmentEncoded);
+    console.log(commitmentHashed);
+    const commitment = BigInt(commitmentHashed) % P;
+    const nonce = await contract.nonces(roundId, nullifier);
+    const { path, sides } = await findMerklePath();
     console.log(path);
     console.log(sides);
-    return path, sides;
+    const input = {
+        secret: userSecret.toString(),
+        siblings: path,
+        sides: sides,
+        commitment: commitment,
+        roundId: roundId.toString(),
+        nonce: nonce.toString()
+    };
+    const commitmentHex = ethers.utils.hexZeroPad("0x" + commitment.toString(16), 32);
+    console.log("secret: ", userSecret.toString());
+    console.log("path: ", path);
+    console.log("sides: ", sides);
+    console.log("commitment: ", commitmentHex);
+    console.log("roundId: ", roundId.toString());
+    console.log("nonce: ", nonce.toString());
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, "voting.wasm", "voting_final.zkey");
+    console.log("public: ", publicSignals);
+    const pA = proof.pi_a.slice(0, 2);
+    const pB = [
+        [proof.pi_b[0][1], proof.pi_b[0][0]],
+        [proof.pi_b[1][1], proof.pi_b[1][0]]
+    ];
+    const pC = proof.pi_c.slice(0, 2);
+
+    console.log("nullifier: ", nullifier);
+    contract.functions.commit(pA, pB, pC, nullifier, commitmentHex, roundId.toString(), nonce.toString());
 }
 
 connectButton.addEventListener('click', logIn);
 getRoundsButton.addEventListener('click', getRounds);
 addRoundButton.addEventListener('click', addRound);
 findPathButton.addEventListener('click', findMerklePath);
+commitButton.addEventListener('click', commit);
